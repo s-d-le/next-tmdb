03948cac25c9425e0756f4b7737900fec9d7d388
Son Le <son.le@expereo.com>
2023-03-04 00:36:20 +0700

    fetch from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+const API_KEY = process.env.TMDB_API_KEY;
 
-const inter = Inter({ subsets: ["latin"] });
+import { Results } from "@/components/Results";
 
-export default function Home() {
-  return <h1 className="text-red-300">Page</h1>;
+export default async function Home({ searchParams }: any) {
+  const genre = searchParams.genre || "fetchTrending";
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 86400 } } //update cache after one day
+  );
+
+  const data = await res.json();
+  const results = data.results;
+  console.log(data);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
---
+++ src/components/Results.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react";
+
+export interface IMovieResult {
+  id: number;
+  title: string;
+}
+
+export interface IMovieResults {
+  results: IMovieResult[];
+}
+
+export const Results: FC<IMovieResults> = ({ results }) => {
+  return (
+    <div>
+      {results.map((result) => (
+        <div key={result.id}>{result.title}</div>
+      ))}
+    </div>
+  );
+};
